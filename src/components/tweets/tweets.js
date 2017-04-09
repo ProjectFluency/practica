44ca673ef73773bd7e7146cac4636a911be9fc31
bcsasankh <bcsasankh@gmail.com>
2017-04-09 15:59:00 -0700

    added base code for chat

--- src/components/tweets/tweets.js
+++ src/components/tweets/tweets.js
@@ -5,6 +5,7 @@ const {
   View,
   Text,
   StyleSheet,
+  TextInput,
   AsyncStorage
 } = ReactNative;
 
@@ -13,7 +14,12 @@ const Button = require('../common/button');
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      username: null
+      username: null,
+      message: '',
+      chat: [
+        "Prashish: hi! love. How are you :)",
+        "girl: I am good love"
+      ]
     };
   },
   componentWillMount: function(){
@@ -34,7 +40,7 @@ module.exports = React.createClass({
 
       return (
         <View style={[styles.container]}>
-          <Text>Loading...</Text>
+        <Text>Loading...</Text>
         </View>
       );
 
@@ -42,12 +48,62 @@ module.exports = React.createClass({
 
       return (
         <View style={[styles.container]}>
-          <Text>Welcome {this.state.username}!!!</Text>
-          <Button text={'Log Out'} onPress={this.onPressLogOut} />
+        <View>
+        <Text>Welcome {this.state.username}!!!</Text>
+        <Button text={'Log Out'} onPress={this.onPressLogOut} />
+        </View>
+
+        <View>
+        <Text style={[styles.label]}>Chat History: </Text>
+        {this.chatHistory()}
+        </View>
+
+        <View>
+        <Text style={[styles.label]}>Enter a message: </Text>
+        <TextInput
+        style={[styles.input]}
+        value={this.state.message}
+        onChangeText={(text) => this.setState({message: text})}
+        />
+        <Button text={'Send'} onPress={this.onPressSend} />
+        </View>
         </View>
       );
 
     }
+  },
+  chatHistory: function() {
+    return this.state.chat.map((message, index) => {
+      return (
+        <View key={index + 1}>
+        <Text>
+        {message}
+        </Text>
+        </View>
+      );
+    });
+  },
+  onPressSend: function(){
+
+//test
+      const newStates = {
+        chat: this.state.chat.concat([this.state.username + " : " + this.state.message])
+      };
+
+      this.setState(newStates)
+//test
+
+    const path = "/message";
+
+    this.props.firebase.database().ref(path).set({message: this.state.message})
+    .then((response) => {
+      this.setState({
+        message: ''
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   },
   onPressLogOut: function() {
     AsyncStorage.removeItem('@guff:username');
@@ -65,5 +121,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
+  },
+  label:{
+    fontSize: 18
   }
 });
